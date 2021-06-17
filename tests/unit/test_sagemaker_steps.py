347885c6e14e555e2579e1c2d9131afb7ac066b6
Carolyn Nguyen <carolngu@amazon.com>
2021-06-17 16:16:18 -0700

    Fix unit test

--- tests/unit/test_sagemaker_steps.py
+++ tests/unit/test_sagemaker_steps.py
@@ -225,7 +225,7 @@ def test_training_step_creation(pca_estimator):
             'TrialName': 'pca_trial',
             'TrialComponentDisplayName': 'Training'
         },
-        output_path='s3://sagemaker-us-east-1-111111111111',
+        output_data_config_path='s3://sagemaker-us-east-1-111111111111',
         tags=DEFAULT_TAGS,
     )
     assert step.to_dict() == {
